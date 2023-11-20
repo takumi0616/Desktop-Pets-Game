34b9d7db392a80358414f19160f68f3c2b72f71e
takasuka_takumi <s233319@stn.nagaokaut.ac.jp>
2023-11-20 17:30:26 +0900

    hfa

--- Desktop-Pets-Game/ContentView.swift
+++ Desktop-Pets-Game/ContentView.swift
@@ -6,21 +6,23 @@
 //  ContentView.swift
 
 import SwiftUI
-import Combine
+import AppKit
 
 struct ContentView: View {
-    @State private var imageNames = ["Image1", "Image2"] // 画像名を配列で管理
-    @State private var currentImageIndex = 0            // 現在の画像のインデックス
-    private let timer = Timer.publish(every: 10, on: .main, in: .common).autoconnect() // 10秒ごとのタイマー
+    @State private var imageNames = (0...7).map { "slice_\($0)_wipe_the_window" }
+    @State private var draggingImageNames = (0...10).map { "slice_\($0)_while_dragging" }
+    @State private var currentImageIndex = 0
+    private let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    @Binding var isDragging: Bool
 
     var body: some View {
-        Image(imageNames[currentImageIndex]) // 現在の画像を表示
+        Image(isDragging ? draggingImageNames[currentImageIndex % draggingImageNames.count] : imageNames[currentImageIndex % imageNames.count])
             .resizable()
             .aspectRatio(contentMode: .fit)
-            .onReceive(timer, perform: { _ in
-                currentImageIndex = (currentImageIndex + 1) % imageNames.count // 画像のインデックスを更新
-            })
             .frame(width: 200, height: 200)
             .background(Color.clear)
+            .onReceive(timer) { _ in
+                currentImageIndex += 1
+            }
     }
 }
--- Desktop-Pets-Game/Desktop_Pets_GameApp.swift
+++ Desktop-Pets-Game/Desktop_Pets_GameApp.swift
@@ -11,30 +11,81 @@ import AppKit
 @main
 struct DesktopPetsGameApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    @State private var isDragging = false
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(isDragging: $isDragging)
         }
     }
 }
 
+
 class AppDelegate: NSObject, NSApplicationDelegate {
-    var window: NSWindow!
-
     func applicationDidFinishLaunching(_ notification: Notification) {
-        let contentView = ContentView()
+        let window = AppWindowManager.shared.build()
+        AppWindowManager.shared.current = window
+    }
+}
 
-        // ウィンドウの設定
-        window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 200, height: 200),
-            styleMask: [.borderless, .fullSizeContentView],
-            backing: .buffered, defer: false)
+class AppWindowManager {
+    static let shared = AppWindowManager()
+    weak var current: DraggableWindow?
+
+    func build() -> DraggableWindow {
+        let rect = NSRect(x: 100, y: 100, width: 300, height: 200)
+        let window = DraggableWindow(contentRect: rect, styleMask: [.borderless, .nonactivatingPanel], backing: .buffered, defer: false)
         window.isOpaque = false
+        window.backgroundColor = NSColor.clear
         window.center()
-        window.setFrameAutosaveName("Desktop Pets Game")
-        window.contentView = NSHostingView(rootView: contentView)
+        // DraggableWindowのインスタンスを保持
+        current = window
+        // Bindingを使用してContentViewを初期化
+        window.contentView = NSHostingView(rootView: ContentView(isDragging: Binding(get: { self.current?.isDragging ?? false }, set: { self.current?.isDragging = $0 })))
+        customize(window)
+        return window
+    }
+    
+    func customize(_ window: NSWindow) {
+        window.collectionBehavior = [.canJoinAllSpaces, .fullScreenAuxiliary]
         window.makeKeyAndOrderFront(nil)
-        window.level = .floating // 常に最前面に表示
+    }
+}
+
+class DraggableWindow: NSPanel, ObservableObject {
+    private var initialLocation: NSPoint?
+    @Published var isDragging = false
+
+    override func mouseDown(with event: NSEvent) {
+        initialLocation = event.locationInWindow
+        isDragging = true  // ドラッグ開始時に状態をtrueに設定
+    }
+
+    override func mouseDragged(with event: NSEvent) {
+        guard let initialLocation = initialLocation else { return }
+        let currentLocation = event.locationInWindow
+        let newOriginX = currentLocation.x - initialLocation.x
+        let newOriginY = currentLocation.y - initialLocation.y
+        self.setFrameOrigin(NSPoint(x: newOriginX, y: newOriginY))
+    }
+
+    override func mouseUp(with event: NSEvent) {
+        isDragging = false  // ドラッグ終了時に状態をfalseに設定
+    }
+    
+    private func updateContentView() {
+        let draggableView = DraggableView()
+        draggableView.addSubview(NSHostingView(rootView: ContentView(isDragging: Binding(get: { self.isDragging }, set: { self.isDragging = $0 }))))
+        self.contentView = draggableView
+    }
+}
+
+class DraggableView: NSView {
+    override func mouseDown(with event: NSEvent) {
+        window?.mouseDown(with: event)
+    }
+
+    override func mouseDragged(with event: NSEvent) {
+        window?.mouseDragged(with: event)
     }
 }
